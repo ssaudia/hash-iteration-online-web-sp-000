c10cfe9895068aa964e3f0651ab40ddaea2ba5c4
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-15 19:22:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,7 @@ def select_winner(passengers)
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
+def age_appropriate_birthday
   end
   winner
 end
@@ -23,8 +24,9 @@ end
 #   "Amanda" => 27
 # }
 
-#def happy_birthday(birthday_kids)
+def happy_birthday(birthday_kids)
   # add your code snippet here!
-  #birthday_kids.each do |kids_name,age|
-    #puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  
+  birthday_kids.each do |kids_name,age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
