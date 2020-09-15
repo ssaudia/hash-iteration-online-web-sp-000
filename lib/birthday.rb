9c309ed5c62a0de382310da48d894007b684b6d2
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-15 19:04:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -26,6 +26,6 @@ end
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name,age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{12} years old!"
   end
 end
